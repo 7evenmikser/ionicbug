576312f38ec7bb4c41a554c8f8eac2ace6108e63
7evenmikser <yyourm@gmail.com>
2020-09-22 20:36:19 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+    
+  </ion-app>
+</template>
+
+<script lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet
+  },
+  data() {
+    return{
+      user: 'x',
+      wtf: ''
+    }
+  },
+  methods:{
+    doTest(){
+      //const a = Plugins.GoogleAuth.signIn({value: 'string'});
+      
+    },
+    do2() {
+      console.log(this)
+      console.log(this.user);
+    }
+
+  }
+});
+</script>
---
+++ src/views/Tab2.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-button @click="do2()">aaa</ion-button>
+      <ExploreContainer name="Tab 2 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import App from '@/App.vue';
+
+export default  {
+  name: 'Tab2',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  methods:{
+    do2(){
+      App.methods.do2();
+    }
+  }
+}
+</script>
